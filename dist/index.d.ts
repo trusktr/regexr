b1585ba68a302667832cd30ddd7306aba5fe69b0
Joe Pea <joe@trusktr.io>
2023-11-01 23:39:23 -0700

    udpate build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,5 +2,5 @@ export declare function regexr<T extends TemplateStringsArray>(literals: T, ...s
 export declare const r: typeof regexr;
 export declare const escape: (string: string) => string;
 export declare const e: (string: string) => string;
-export declare const version = "2.0.2";
+export declare const version = "2.0.3";
 //# sourceMappingURL=index.d.ts.map
